INKLUSIVACall/inklusiva-meet

commit 9753276f866bf785c134119b3bb20f72ac5ebf47

Run simulcastlayerschanged/ing event handlers only if the affected endpoint is in lastN.

videolayout.js
@@ -1542,6 +1542,12 @@ var VideoLayout = (function (my) {
 
     $(document).bind('simulcastlayerschanging', function (event, endpointSimulcastLayers) {
         endpointSimulcastLayers.forEach(function (esl) {
+
+            var resource = esl.endpoint;
+            if (lastNCount < 1 || lastNEndpointsCache.indexOf(resource) === -1) {
+                return;
+            }
+
             var primarySSRC = esl.simulcastLayer.primarySSRC;
 
             // Get session and stream from primary ssrc.
@@ -1588,6 +1594,11 @@ var VideoLayout = (function (my) {
      */
     $(document).bind('simulcastlayerschanged', function (event, endpointSimulcastLayers) {
         endpointSimulcastLayers.forEach(function (esl) {
+
+            var resource = esl.endpoint;
+            if (lastNCount < 1 || lastNEndpointsCache.indexOf(resource) === -1) {
+                return;
+            }
 
             var primarySSRC = esl.simulcastLayer.primarySSRC;
 
@@ -1644,15 +1655,14 @@ var VideoLayout = (function (my) {
                     focusedVideoSrc = electedStreamUrl;
                 }
 
-                var jid = ssrc2jid[primarySSRC];
                 var videoId;
-                if(jid == connection.emuc.myroomjid)
+                if(resource == Strophe.getResourceFromJid(connection.emuc.myroomjid))
                 {
                     videoId = "localVideoContainer";
                 }
                 else
                 {
-                    videoId = "participant_" + Strophe.getResourceFromJid(jid);
+                    videoId = "participant_" + resource;
                 }
                 var connectionIndicator = VideoLayout.connectionIndicators[videoId];
                 if(connectionIndicator)

commit d01d8841ced88444ac7bb58dce1ae3709a118808

Fixes issue introduced by 9753276f866bf785c134119b3bb20f72ac5ebf47.
simulcastlayerschanged/ing should run if lastN is disabled.

videolayout.js
@@ -1640,7 +1640,15 @@ var VideoLayout = (function (my) {
         endpointSimulcastLayers.forEach(function (esl) {
 
             var resource = esl.endpoint;
-            if (lastNCount < 1 || lastNEndpointsCache.indexOf(resource) === -1) {
+
+            // if lastN is enabled *and* the endpoint is *not* in the lastN set,
+            // then ignore the event (= do not preload anything).
+            //
+            // The bridge could probably stop sending this message if it's for
+            // an endpoint that's not in lastN.
+
+            if (lastNCount != -1
+                && (lastNCount < 1 || lastNEndpointsCache.indexOf(resource) === -1)) {
                 return;
             }
 
@@ -1692,7 +1700,19 @@ var VideoLayout = (function (my) {
         endpointSimulcastLayers.forEach(function (esl) {
 
             var resource = esl.endpoint;
-            if (lastNCount < 1 || lastNEndpointsCache.indexOf(resource) === -1) {
+
+            // if lastN is enabled *and* the endpoint is *not* in the lastN set,
+            // then ignore the event (= do not change large video/thumbnail
+            // SRCs).
+            //
+            // Note that even if we ignore the "changing" event in this event
+            // handler, the bridge must continue sending these events because
+            // the simulcast code in simulcast.js handles it to know what's
+            // going to be streamed by the bridge when/if the endpoint gets back
+            // into the lastN set.
+
+            if (lastNCount != -1
+                && (lastNCount < 1 || lastNEndpointsCache.indexOf(resource) === -1)) {
                 return;
             }
 