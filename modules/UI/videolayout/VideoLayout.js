jitsi/jitsi-meet

Fixes a failure to show the dominant speaker indicator for the local participant.

modules/UI/videolayout/VideoLayout.js
@@ -590,9 +590,10 @@ var VideoLayout = {
             if(oldSpeakerRemoteVideo)
             {
                 oldSpeakerRemoteVideo.showDominantSpeakerIndicator(false);
-                localVideoThumbnail.showDominantSpeakerIndicator(true);
+                // XXX Why do we not set this to id?
                 currentDominantSpeaker = null;
             }
+            localVideoThumbnail.showDominantSpeakerIndicator(true);
             return;
         }
 

INKLUSIVACall/inklusiva-meet

modules/UI/videolayout/VideoLayout.js
@@ -577,9 +577,9 @@ var VideoLayout = {
             if(oldSpeakerRemoteVideo)
             {
                 oldSpeakerRemoteVideo.updateDominantSpeakerIndicator(false);
-                localVideoThumbnail.updateDominantSpeakerIndicator(true);
                 currentDominantSpeaker = null;
             }
+            localVideoThumbnail.updateDominantSpeakerIndicator(true);
             return;
         }
 