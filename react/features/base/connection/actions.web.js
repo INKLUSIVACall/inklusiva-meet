jitsi/jitsi-meet

Rename style component; Use of status codes instead of flags in conference init

react/features/base/connection/actions.web.js
@@ -97,7 +97,7 @@ export function connect() {
                         // Setting plugin required flag to false because
                         // it's already been installed.
                         dispatch(setUnsupportedBrowser({
-                            isPluginRequired: false
+                            status: 'OK'
                         }));
                         dispatch(appNavigate(room));
                     });

INKLUSIVACall/inklusiva-meet

react/features/base/connection/actions.web.js
@@ -95,7 +95,7 @@ export function connect() {
                         // Setting plugin required flag to false because
                         // it's already been installed.
                         dispatch(setUnsupportedBrowser({
-                            isPluginRequired: false
+                            name: 'OK'
                         }));
                         dispatch(appNavigate(room));
                     });