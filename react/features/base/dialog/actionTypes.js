vunb/jitsi-meet

Adds base dialog implementation.

react/features/base/dialog/actionTypes.js
@@ -1,0 +1,22 @@
+import { Symbol } from '../react';
+
+/**
+ * The type of Redux action which begins a request to open a dialog.
+ *
+ * {
+ *     type: BEGIN_DIALOG_REQUEST,
+ *     component: React.Component,
+ *     props: React.PropTypes.object
+ * }
+ *
+ */
+export const BEGIN_DIALOG_REQUEST = Symbol('BEGIN_DIALOG_REQUEST');
+
+/**
+ * The type of Redux action which closes a dialog
+ *
+ * {
+ *     type: END_DIALOG_REQUEST
+ * }
+ */
+export const END_DIALOG_REQUEST = Symbol('END_DIALOG_REQUEST');

INKLUSIVACall/inklusiva-meet

react/features/base/dialog/actionTypes.js
@@ -1,0 +1,22 @@
+import { Symbol } from '../react';
+
+/**
+ * The type of Redux action which closes a dialog
+ *
+ * {
+ *     type: HIDE_DIALOG
+ * }
+ */
+export const HIDE_DIALOG = Symbol('HIDE_DIALOG');
+
+/**
+ * The type of Redux action which begins a request to open a dialog.
+ *
+ * {
+ *     type: OPEN_DIALOG,
+ *     component: React.Component,
+ *     props: React.PropTypes.object
+ * }
+ *
+ */
+export const OPEN_DIALOG = Symbol('OPEN_DIALOG');