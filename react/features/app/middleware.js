jitsi/jitsi-meet

[RN] Rename createInitialLocalTracks to createLocalTracks
The name better suits its purpose, since it can be called at any time.

react/features/app/middleware.js
@@ -5,7 +5,7 @@ import {
     SET_LOCATION_URL
 } from '../base/connection';
 import { MiddlewareRegistry } from '../base/redux';
-import { createInitialLocalTracks, destroyLocalTracks } from '../base/tracks';
+import { createLocalTracks, destroyLocalTracks } from '../base/tracks';
 
 MiddlewareRegistry.register(store => next => action => {
     switch (action.type) {
@@ -99,7 +99,7 @@ function _navigate({ dispatch, getState }) {
         } else {
             // Create the local tracks if they haven't been created yet.
             state['features/base/tracks'].some(t => t.local)
-                || dispatch(createInitialLocalTracks());
+                || dispatch(createLocalTracks());
         }
     }
 

react/features/base/tracks/functions.js
@@ -23,7 +23,7 @@ const logger = require('jitsi-meet-logger').getLogger(__filename);
  * is to execute and from which state such as {@code config} is to be retrieved.
  * @returns {Promise<JitsiLocalTrack[]>}
  */
-export function createLocalTracks(
+export function _createLocalTracks(
         options,
         firePermissionPromptIsShownEvent,
         store) {

INKLUSIVACall/inklusiva-meet

react/features/app/middleware.js
@@ -5,7 +5,7 @@ import {
     SET_LOCATION_URL
 } from '../base/connection';
 import { MiddlewareRegistry } from '../base/redux';
-import { createInitialLocalTracks, destroyLocalTracks } from '../base/tracks';
+import { createLocalTracksA, destroyLocalTracks } from '../base/tracks';
 
 MiddlewareRegistry.register(store => next => action => {
     switch (action.type) {
@@ -99,7 +99,7 @@ function _navigate({ dispatch, getState }) {
         } else {
             // Create the local tracks if they haven't been created yet.
             state['features/base/tracks'].some(t => t.local)
-                || dispatch(createInitialLocalTracks());
+                || dispatch(createLocalTracksA());
         }
     }
 

react/features/base/tracks/functions.js
@@ -23,7 +23,7 @@ const logger = require('jitsi-meet-logger').getLogger(__filename);
  * is to execute and from which state such as {@code config} is to be retrieved.
  * @returns {Promise<JitsiLocalTrack[]>}
  */
-export function createLocalTracks(
+export function createLocalTracksF(
         options,
         firePermissionPromptIsShownEvent,
         store) {