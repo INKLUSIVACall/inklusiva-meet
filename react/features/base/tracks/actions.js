jitsi/jitsi-meet

[RN] Create tracks right when they are required
When do we need tracks?

- Welcome page (only the video track)
- Conference (depends if starting with audio / video muted is requested)

When do we need to destroy the tracks?

- When we are not in a conference and there is no welcome page

In order to accommodate all the above use cases, a new component is introduced:
BlankWelcomePage. Its purpose is to take the place of the welcome page when it
is disabled. When this component is mounted local tracks are destroyed.

Analogously, a video track is created when the (real) welcome page is created,
and all the desired tracks are created then the Conference component is created.
What are desired tracks? These are the tracks we'd like to use for the
conference that is about to happen. By default both audio and video are desired.
It's possible, however, the user requested to start the call with no
video/audio, in which case it's muted in base/media and a track is not created.

The first time the app starts (with the welcome page) it will request permission
for video only, since there is no need for audio in the welcome page. Later,
when a conference is joined permission for audio will be requested when an audio
track is to be created. The audio track is not destroyed when the conference
ends. Yours truly thinks this is not needed since it's a stopped track which is
not using system resources.

react/features/base/tracks/actions.js
@@ -11,6 +11,37 @@ import { TRACK_ADDED, TRACK_REMOVED, TRACK_UPDATED } from './actionTypes';
 import { _createLocalTracks } from './functions';
 
 /**
+ * Requests the creating of the desired media type tracks. Desire is expressed
+ * by base/media. This function will dispatch a {@code createLocalTracks}
+ * action for the "missing" types, that is, the ones which base/media would
+ * like to have (unmuted tracks) but are not present yet.
+ *
+ * @returns {Function}
+ */
+export function createDesiredLocalTracks() {
+    return (dispatch, getState) => {
+        const state = getState();
+        const desiredTypes = [];
+
+        state['features/base/media'].audio.muted
+            || desiredTypes.push(MEDIA_TYPE.AUDIO);
+        Boolean(state['features/base/media'].video.muted)
+            || desiredTypes.push(MEDIA_TYPE.VIDEO);
+
+        const availableTypes
+            = state['features/base/tracks']
+                .filter(t => t.local).map(t => t.mediaType);
+
+        // We need to create the desired tracks which are not already available.
+        const createTypes
+            = desiredTypes.filter(type => availableTypes.indexOf(type) === -1);
+
+        createTypes.length
+            && dispatch(createLocalTracks({ devices: createTypes }));
+    };
+}
+
+/**
  * Request to start capturing local audio and/or video. By default, the user
  * facing camera will be selected.
  *

react/features/welcome/components/WelcomePage.native.js
@@ -3,8 +3,10 @@ import { TextInput, TouchableHighlight, View } from 'react-native';
 import { connect } from 'react-redux';
 
 import { translate } from '../../base/i18n';
+import { MEDIA_TYPE } from '../../base/media';
 import { Link, Text } from '../../base/react';
 import { ColorPalette } from '../../base/styles';
+import { createLocalTracks } from '../../base/tracks';
 
 import { AbstractWelcomePage, _mapStateToProps } from './AbstractWelcomePage';
 import styles from './styles';
@@ -35,7 +37,20 @@ class WelcomePage extends AbstractWelcomePage {
      *
      * @static
      */
-    static propTypes = AbstractWelcomePage.propTypes
+    static propTypes = AbstractWelcomePage.propTypes;
+
+    /**
+     * Creates a video track if not already available.
+     *
+     * @inheritdoc
+     * @returns {void}
+     */
+    componentWillMount() {
+        const { dispatch, _localVideoTrack } = this.props;
+
+        (typeof _localVideoTrack === 'undefined')
+            && dispatch(createLocalTracks({ devices: [ MEDIA_TYPE.VIDEO ] }));
+    }
 
     /**
      * Renders a prompt for entering a room name.

INKLUSIVACall/inklusiva-meet

react/features/base/tracks/actions.js
@@ -11,6 +11,38 @@ import { TRACK_ADDED, TRACK_REMOVED, TRACK_UPDATED } from './actionTypes';
 import { createLocalTracksF } from './functions';
 
 /**
+ * Requests the creating of the desired media type tracks. Desire is expressed
+ * by base/media. This function will dispatch a {@code createLocalTracksA}
+ * action for the "missing" types, that is, the ones which base/media would
+ * like to have (unmuted tracks) but are not present yet.
+ *
+ * @returns {Function}
+ */
+export function createDesiredLocalTracks() {
+    return (dispatch, getState) => {
+        const state = getState();
+        const desiredTypes = [];
+
+        state['features/base/media'].audio.muted
+            || desiredTypes.push(MEDIA_TYPE.AUDIO);
+        Boolean(state['features/base/media'].video.muted)
+            || desiredTypes.push(MEDIA_TYPE.VIDEO);
+
+        const availableTypes
+            = state['features/base/tracks']
+                .filter(t => t.local)
+                .map(t => t.mediaType);
+
+        // We need to create the desired tracks which are not already available.
+        const createTypes
+            = desiredTypes.filter(type => availableTypes.indexOf(type) === -1);
+
+        createTypes.length
+            && dispatch(createLocalTracksA({ devices: createTypes }));
+    };
+}
+
+/**
  * Request to start capturing local audio and/or video. By default, the user
  * facing camera will be selected.
  *

react/features/welcome/components/WelcomePage.native.js
@@ -3,8 +3,10 @@ import { TextInput, TouchableHighlight, View } from 'react-native';
 import { connect } from 'react-redux';
 
 import { translate } from '../../base/i18n';
+import { MEDIA_TYPE } from '../../base/media';
 import { Link, Text } from '../../base/react';
 import { ColorPalette } from '../../base/styles';
+import { createLocalTracksA } from '../../base/tracks';
 
 import { AbstractWelcomePage, _mapStateToProps } from './AbstractWelcomePage';
 import styles from './styles';
@@ -35,7 +37,20 @@ class WelcomePage extends AbstractWelcomePage {
      *
      * @static
      */
-    static propTypes = AbstractWelcomePage.propTypes
+    static propTypes = AbstractWelcomePage.propTypes;
+
+    /**
+     * Creates a video track if not already available.
+     *
+     * @inheritdoc
+     * @returns {void}
+     */
+    componentWillMount() {
+        const { dispatch, _localVideoTrack } = this.props;
+
+        (typeof _localVideoTrack === 'undefined')
+            && dispatch(createLocalTracksA({ devices: [ MEDIA_TYPE.VIDEO ] }));
+    }
 
     /**
      * Renders a prompt for entering a room name.