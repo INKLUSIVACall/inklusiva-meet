TusharPatilGithub/jitsi-meet

feat(mute): Add analytics and console logs for audio/video mutes

conference.js
@@ -674,9 +674,13 @@ export default {
             }).then(([tracks, con]) => {
                 tracks.forEach(track => {
                     if (track.isAudioTrack() && this.isLocalAudioMuted()) {
+                        sendEvent('conference.audio.initiallyMuted');
+                        logger.log('Audio mute: initially muted');
                         track.mute();
                     } else if (track.isVideoTrack()
                                     && this.isLocalVideoMuted()) {
+                        sendEvent('conference.video.initiallyMuted');
+                        logger.log('Video mute: initially muted');
                         track.mute();
                     }
                 });
@@ -2260,6 +2264,8 @@ export default {
                     if (audioWasMuted ||
                         currentDevices.audioinput.length >
                         availableAudioInputDevices.length) {
+                        sendEvent('deviceListChanged.audio.mute');
+                        logger.log('Audio mute: device list changed');
                         muteLocalAudio(true);
                     }
 
@@ -2269,6 +2275,8 @@ export default {
                         (videoWasMuted ||
                             currentDevices.videoinput.length >
                                 availableVideoInputDevices.length)) {
+                        sendEvent('deviceListChanged.video.mute');
+                        logger.log('Video mute: device list changed');
                         muteLocalVideo(true);
                     }
                 }));

modules/UI/shared_video/SharedVideo.js
@@ -533,7 +533,8 @@ export default class SharedVideoManager {
         if (APP.conference.isLocalAudioMuted()
             && !this.mutedWithUserInteraction
             && !this.isSharedVideoVolumeOn()) {
-
+            sendEvent("sharedvideo.audio.unmute");
+            logger.log('Shared video: audio unmute');
             this.emitter.emit(UIEvents.AUDIO_MUTED, false, false);
             this.showMicMutedPopup(false);
         }
@@ -546,7 +547,8 @@ export default class SharedVideoManager {
     smartAudioMute() {
         if (!APP.conference.isLocalAudioMuted()
             && this.isSharedVideoVolumeOn()) {
-
+            sendEvent("sharedvideo.audio.mute");
+            logger.log('Shared video: audio mute');
             this.emitter.emit(UIEvents.AUDIO_MUTED, true, false);
             this.showMicMutedPopup(true);
         }

react/features/mobile/callkit/middleware.js
@@ -3,6 +3,7 @@
 import { NativeModules } from 'react-native';
 import uuid from 'uuid';
 
+import { sendEvent } from '../../analytics';
 import { APP_WILL_MOUNT, APP_WILL_UNMOUNT, appNavigate } from '../../app';
 import {
     CONFERENCE_FAILED,
@@ -264,7 +265,10 @@ function _onPerformSetMutedCallAction({ callUUID, muted: newValue }) {
         const { muted: oldValue } = getState()['features/base/media'].audio;
 
         if (oldValue !== newValue) {
-            dispatch(setAudioMuted(Boolean(newValue)));
+            const value = Boolean(newValue);
+
+            sendEvent(`callkit.audio.${value ? 'mute' : 'unmute'}`);
+            dispatch(setAudioMuted(value));
         }
     }
 }

INKLUSIVACall/inklusiva-meet

conference.js
@@ -674,9 +674,13 @@ export default {
             }).then(([tracks, con]) => {
                 tracks.forEach(track => {
                     if (track.isAudioTrack() && this.isLocalAudioMuted()) {
+                        sendEvent('conference.audio.initiallyMuted');
+                        logger.log('Audio mute: initially muted');
                         track.mute();
                     } else if (track.isVideoTrack()
                                     && this.isLocalVideoMuted()) {
+                        sendEvent('conference.video.initiallyMuted');
+                        logger.log('Video mute: initially muted');
                         track.mute();
                     }
                 });
@@ -2260,6 +2264,8 @@ export default {
                     if (audioWasMuted ||
                         currentDevices.audioinput.length >
                         availableAudioInputDevices.length) {
+                        sendEvent('deviceListChanged.audio.muted');
+                        logger.log('Audio mute: device list changed');
                         muteLocalAudio(true);
                     }
 
@@ -2269,6 +2275,8 @@ export default {
                         (videoWasMuted ||
                             currentDevices.videoinput.length >
                                 availableVideoInputDevices.length)) {
+                        sendEvent('deviceListChanged.video.muted');
+                        logger.log('Video mute: device list changed');
                         muteLocalVideo(true);
                     }
                 }));

modules/UI/shared_video/SharedVideo.js
@@ -533,7 +533,8 @@ export default class SharedVideoManager {
         if (APP.conference.isLocalAudioMuted()
             && !this.mutedWithUserInteraction
             && !this.isSharedVideoVolumeOn()) {
-
+            sendEvent("sharedvideo.audio.unmuted");
+            logger.log('Shared video: audio unmuted');
             this.emitter.emit(UIEvents.AUDIO_MUTED, false, false);
             this.showMicMutedPopup(false);
         }
@@ -546,7 +547,8 @@ export default class SharedVideoManager {
     smartAudioMute() {
         if (!APP.conference.isLocalAudioMuted()
             && this.isSharedVideoVolumeOn()) {
-
+            sendEvent("sharedvideo.audio.muted");
+            logger.log('Shared video: audio muted');
             this.emitter.emit(UIEvents.AUDIO_MUTED, true, false);
             this.showMicMutedPopup(true);
         }

react/features/mobile/callkit/middleware.js
@@ -3,6 +3,7 @@
 import { NativeModules } from 'react-native';
 import uuid from 'uuid';
 
+import { sendEvent } from '../../analytics';
 import { APP_WILL_MOUNT, APP_WILL_UNMOUNT, appNavigate } from '../../app';
 import {
     CONFERENCE_FAILED,
@@ -268,7 +269,10 @@ function _onPerformSetMutedCallAction({ callUUID, muted: newValue }) {
         const { muted: oldValue } = getState()['features/base/media'].audio;
 
         if (oldValue !== newValue) {
-            dispatch(setAudioMuted(Boolean(newValue)));
+            const value = Boolean(newValue);
+
+            sendEvent(`callkit.audio.${value ? 'muted' : 'unmuted'}`);
+            dispatch(setAudioMuted(value));
         }
     }
 }