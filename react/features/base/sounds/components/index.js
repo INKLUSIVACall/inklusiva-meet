shikhar-scs/jitsi-meet

feat: add join/leave sounds on mobile
Adds base/sounds feature which allows other features to register a sound
source under specified id. A new SoundsCollection component will then
render corresponding HTMLAudioElement for each such sound. Once "setRef"
callback is called by the HTMLAudioElement, this element will be added
to the Redux store. When that happens sound can be played through the
new 'playSound' action which will call play() method on the stored
HTMLAudioElement instance.

react/features/base/sounds/components/index.js
@@ -1,0 +1,1 @@
+export { default as SoundsCollection } from './SoundsCollection';

INKLUSIVACall/inklusiva-meet

react/features/base/sounds/components/index.js
@@ -1,0 +1,1 @@
+export { default as SoundCollection } from './SoundCollection';