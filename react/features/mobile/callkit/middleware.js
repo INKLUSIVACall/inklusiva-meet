INKLUSIVACall/inklusiva-meet

commit 486d20992dda9a00343ee55565ac3481d2f78451

[iOS] Refactor muted state handling in CallKit
Rely solely on actual track state, rather than the desired state, (what
base/media represents).

react/features/mobile/callkit/middleware.js
@@ -19,13 +19,16 @@ import {
 import { getInviteURL } from '../../base/connection';
 import {
     MEDIA_TYPE,
-    SET_AUDIO_MUTED,
-    SET_VIDEO_MUTED,
     isVideoMutedByAudioOnly,
     setAudioMuted
 } from '../../base/media';
 import { MiddlewareRegistry } from '../../base/redux';
-import { TRACK_CREATE_ERROR, isLocalTrackMuted } from '../../base/tracks';
+import {
+    TRACK_ADDED,
+    TRACK_REMOVED,
+    TRACK_UPDATED,
+    isLocalTrackMuted
+} from '../../base/tracks';
 
 import { _SET_CALLKIT_SUBSCRIPTIONS } from './actionTypes';
 import CallKit from './CallKit';
@@ -63,14 +66,10 @@ CallKit && MiddlewareRegistry.register(store => next => action => {
     case CONFERENCE_WILL_JOIN:
         return _conferenceWillJoin(store, next, action);
 
-    case SET_AUDIO_MUTED:
-        return _setAudioMuted(store, next, action);
-
-    case SET_VIDEO_MUTED:
-        return _setVideoMuted(store, next, action);
-
-    case TRACK_CREATE_ERROR:
-        return _trackCreateError(store, next, action);
+    case TRACK_ADDED:
+    case TRACK_REMOVED:
+    case TRACK_UPDATED:
+        return _syncTrackState(store, next, action);
     }
 
     return next(action);
@@ -293,38 +292,13 @@ function _onPerformSetMutedCallAction({ callUUID, muted: newValue }) {
         const tracks = getState()['features/base/tracks'];
         const oldValue = isLocalTrackMuted(tracks, MEDIA_TYPE.AUDIO);
 
-        if (oldValue !== newValue) {
-            const value = Boolean(newValue);
+        newValue = Boolean(newValue); // eslint-disable-line no-param-reassign
 
-            sendAnalytics(createTrackMutedEvent('audio', 'callkit', value));
-            dispatch(setAudioMuted(value, /* ensureTrack */ true));
+        if (oldValue !== newValue) {
+            sendAnalytics(createTrackMutedEvent('audio', 'callkit', newValue));
+            dispatch(setAudioMuted(newValue, /* ensureTrack */ true));
         }
     }
-}
-
-/**
- * Notifies the feature callkit that the action {@link SET_AUDIO_MUTED} is being
- * dispatched within a specific redux {@code store}.
- *
- * @param {Store} store - The redux store in which the specified {@code action}
- * is being dispatched.
- * @param {Dispatch} next - The redux {@code dispatch} function to dispatch the
- * specified {@code action} in the specified {@code store}.
- * @param {Action} action - The redux action {@code SET_AUDIO_MUTED} which is
- * being dispatched in the specified {@code store}.
- * @private
- * @returns {*} The value returned by {@code next(action)}.
- */
-function _setAudioMuted({ getState }, next, action) {
-    const result = next(action);
-
-    const conference = getCurrentConference(getState);
-
-    if (conference && conference.callUUID) {
-        CallKit.setMuted(conference.callUUID, action.muted);
-    }
-
-    return result;
 }
 
 /**
@@ -354,57 +328,31 @@ function _setCallKitSubscriptions({ getState }, next, action) {
 }
 
 /**
- * Notifies the feature callkit that the action {@link SET_VIDEO_MUTED} is being
- * dispatched within a specific redux {@code store}.
+ * Synchronize the muted state of tracks with CallKit.
  *
  * @param {Store} store - The redux store in which the specified {@code action}
  * is being dispatched.
  * @param {Dispatch} next - The redux {@code dispatch} function to dispatch the
  * specified {@code action} in the specified {@code store}.
- * @param {Action} action - The redux action {@code SET_VIDEO_MUTED} which is
- * being dispatched in the specified {@code store}.
+ * @param {Action} action - The redux action which is being dispatched in the
+ * specified {@code store}.
  * @private
  * @returns {*} The value returned by {@code next(action)}.
  */
-function _setVideoMuted({ getState }, next, action) {
+function _syncTrackState({ getState }, next, action) {
     const result = next(action);
-
-    const conference = getCurrentConference(getState);
-
-    if (conference && conference.callUUID) {
-        CallKit.updateCall(
-            conference.callUUID,
-            { hasVideo: !isVideoMutedByAudioOnly(getState) });
-    }
-
-    return result;
-}
-
-/**
- * Handles a track creation failure. This is relevant to us in the following
- * (corner) case: if the user never gave their permission to use the microphone
- * and try to unmute from the CallKit interface, this will fail, and we need to
- * sync back the CallKit button state.
- *
- * @param {Store} store - The redux store in which the specified {@code action}
- * is being dispatched.
- * @param {Dispatch} next - The redux {@code dispatch} function to dispatch the
- * specified {@code action} in the specified {@code store}.
- * @param {Action} action - The redux action {@code TRACK_CREARE_ERROR} which is
- * being dispatched in the specified {@code store}.
- * @private
- * @returns {*} The value returned by {@code next(action)}.
- */
-function _trackCreateError({ getState }, next, action) {
-    const result = next(action);
+    const { track } = action;
     const state = getState();
     const conference = getCurrentConference(state);
 
-    if (conference && conference.callUUID) {
+    if (track.local && conference && conference.callUUID) {
         const tracks = state['features/base/tracks'];
         const muted = isLocalTrackMuted(tracks, MEDIA_TYPE.AUDIO);
 
         CallKit.setMuted(conference.callUUID, muted);
+        CallKit.updateCall(
+            conference.callUUID,
+            { hasVideo: !isVideoMutedByAudioOnly(state) });
     }
 
     return result;

commit 60dc9aa5d6cdf0df968cf04bdef615969ca25e7d

[iOS] Fix setting call type in CallKit
Your truly introduced this regression in
486d20992dda9a00343ee55565ac3481d2f78451, alas.

The audio only mode is used to set the CallKit call type. This affects the
behavior on the recent calls entries (calls are marked either as audio or video
calls).

Sync both at the start and for transitions. The previous code was working by
chance (in a way): when the CallKit UI is presented the local video is muted,
which triggers a SET_VIDEO_MUTED action, at which point the audio-only mode was
checked for. Now we are more explicit and act on SET_AUDIO_MUTED.

react/features/mobile/callkit/middleware.js
@@ -14,6 +14,7 @@ import {
     CONFERENCE_LEFT,
     CONFERENCE_WILL_JOIN,
     CONFERENCE_JOINED,
+    SET_AUDIO_ONLY,
     getCurrentConference
 } from '../../base/conference';
 import { getInviteURL } from '../../base/connection';
@@ -65,6 +66,9 @@ CallKit && MiddlewareRegistry.register(store => next => action => {
 
     case CONFERENCE_WILL_JOIN:
         return _conferenceWillJoin(store, next, action);
+
+    case SET_AUDIO_ONLY:
+        return _setAudioOnly(store, next, action);
 
     case TRACK_ADDED:
     case TRACK_REMOVED:
@@ -247,7 +251,8 @@ function _conferenceWillJoin({ getState }, next, action) {
                     state['features/base/tracks'],
                     MEDIA_TYPE.AUDIO);
 
-            CallKit.updateCall(conference.callUUID, { displayName });
+            // eslint-disable-next-line object-property-newline
+            CallKit.updateCall(conference.callUUID, { displayName, hasVideo });
             CallKit.setMuted(conference.callUUID, muted);
         });
 
@@ -299,6 +304,40 @@ function _onPerformSetMutedCallAction({ callUUID, muted: newValue }) {
             dispatch(setAudioMuted(newValue, /* ensureTrack */ true));
         }
     }
+}
+
+/**
+ * Update CallKit with the audio only state of the conference. When a conference
+ * is in audio only mode we will tell CallKit the call has no video. This
+ * affects how the call is saved in the recent calls list.
+ *
+ * XXX: Note that here we are taking the `audioOnly` value straight from the
+ * action, instead of examining the state. This is intentional, as setting the
+ * audio only involves multiple actions which will be reflected in the state
+ * later, but we are just interested in knowing if the mode is going to be
+ * set or not.
+ *
+ * @param {Store} store - The redux store in which the specified {@code action}
+ * is being dispatched.
+ * @param {Dispatch} next - The redux {@code dispatch} function to dispatch the
+ * specified {@code action} in the specified {@code store}.
+ * @param {Action} action - The redux action which is being dispatched in the
+ * specified {@code store}.
+ * @private
+ * @returns {*} The value returned by {@code next(action)}.
+ */
+function _setAudioOnly({ getState }, next, action) {
+    const result = next(action);
+    const state = getState();
+    const conference = getCurrentConference(state);
+
+    if (conference && conference.callUUID) {
+        CallKit.updateCall(
+            conference.callUUID,
+            { hasVideo: !action.audioOnly });
+    }
+
+    return result;
 }
 
 /**