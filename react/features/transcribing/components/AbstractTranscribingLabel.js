Tropeas/jitsi-meet

feat(transcriptions): add TranscribingLabel.native

react/features/transcribing/components/AbstractTranscribingLabel.js
@@ -1,0 +1,12 @@
+// @flow
+
+/**
+ * The type of the React {@code Component} props of {@link TranscribingLabel}.
+ */
+export type AbstractProps = {
+
+    /**
+     * Invoked to obtain translated strings.
+     */
+    t: Function
+};

INKLUSIVACall/inklusiva-meet

react/features/transcribing/components/AbstractTranscribingLabel.js
@@ -1,0 +1,12 @@
+// @flow
+
+/**
+ * The type of the React {@code Component} props of {@link TranscribingLabel}.
+ */
+export type Props = {
+
+    /**
+     * Invoked to obtain translated strings.
+     */
+    t: Function
+};