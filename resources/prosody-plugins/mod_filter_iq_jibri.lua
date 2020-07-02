zhaxun/jitsi-meet

feat: Adds an option to validate a recording token.

resources/prosody-plugins/mod_filter_iq_jibri.lua
@@ -1,5 +1,8 @@
 local st = require "util.stanza";
 local is_feature_allowed = module:require "util".is_feature_allowed;
+local token_util = module:require "token/util".new(parentCtx);
+
+local accepted_rayo_iq_token_issuers = module:get_option_string("accepted_rayo_iq_token_issuers");
 
 -- filters jibri iq in case of requested from jwt authenticated session that
 -- has features in the user context, but without feature for recording
@@ -11,15 +14,39 @@ module:hook("pre-iq/full", function(event)
             local session = event.origin;
             local token = session.auth_token;
 
-            if jibri.attr.action == 'start'
-                and (token == nil
+            if jibri.attr.action == 'start' then
+                if accepted_rayo_iq_token_issuers then
+                    local app_data = jibri.attr.app_data;
+                    local errorReason = '';
+                    if app_data then
+                        app_data = json.decode(app_data);
+                        module:log("debug","jibri app data found %s",inspect(app_data));
+
+                        if app_data.token then
+                            local session = {};
+                            session.auth_token = app_data.token;
+                            local verified, reason = token_util:process_and_verify_token(session, accepted_rayo_iq_token_issuers);
+                            if verified then
+                                return nil; -- this will proceed with dispatching the stanza
+                            end
+                            errorReason = reason;
+                        end
+                    end
+
+                    log("warn", "not a valid token %s", tostring(errorReason));
+                    session.send(st.error_reply(stanza, "auth", "forbidden"));
+                    return true;
+                end
+
+                if token == nil
                     or not is_feature_allowed(session,
-                        (jibri.attr.recording_mode == 'file' and 'recording' or 'livestreaming'))
+                    (jibri.attr.recording_mode == 'file' and 'recording' or 'livestreaming')
                 ) then
-                module:log("info",
-                    "Filtering jibri start recording, stanza:%s", tostring(stanza));
-                session.send(st.error_reply(stanza, "auth", "forbidden"));
-                return true;
+                    module:log("info",
+                        "Filtering jibri start recording, stanza:%s", tostring(stanza));
+                    session.send(st.error_reply(stanza, "auth", "forbidden"));
+                    return true;
+                end
             end
         end
     end

INKLUSIVACall/inklusiva-meet

resources/prosody-plugins/mod_filter_iq_jibri.lua
@@ -1,5 +1,8 @@
 local st = require "util.stanza";
 local is_feature_allowed = module:require "util".is_feature_allowed;
+local token_util = module:require "token/util".new(module);
+
+local accepted_rayo_iq_token_issuers = module:get_option_array("accepted_rayo_iq_token_issuers");
 
 -- filters jibri iq in case of requested from jwt authenticated session that
 -- has features in the user context, but without feature for recording
@@ -11,15 +14,37 @@ module:hook("pre-iq/full", function(event)
             local session = event.origin;
             local token = session.auth_token;
 
-            if jibri.attr.action == 'start'
-                and (token == nil
+            if jibri.attr.action == 'start' then
+                local errorReason;
+                if accepted_rayo_iq_token_issuers then
+                    local iq_token = jibri.attr.token;
+                    if iq_token then
+                        local session = {};
+                        session.auth_token = iq_token;
+                        local verified, reason = token_util:process_and_verify_token(
+                            session, accepted_rayo_iq_token_issuers);
+                        if verified then
+                            return nil; -- this will proceed with dispatching the stanza
+                        end
+                        errorReason = reason;
+                    else
+                        errorReason = 'No recording token provided';
+                    end
+
+                    module:log("warn", "not a valid token %s", tostring(errorReason));
+                    session.send(st.error_reply(stanza, "auth", "forbidden"));
+                    return true;
+                end
+
+                if token == nil
                     or not is_feature_allowed(session,
-                        (jibri.attr.recording_mode == 'file' and 'recording' or 'livestreaming'))
+                    (jibri.attr.recording_mode == 'file' and 'recording' or 'livestreaming')
                 ) then
-                module:log("info",
-                    "Filtering jibri start recording, stanza:%s", tostring(stanza));
-                session.send(st.error_reply(stanza, "auth", "forbidden"));
-                return true;
+                    module:log("info",
+                        "Filtering jibri start recording, stanza:%s", tostring(stanza));
+                    session.send(st.error_reply(stanza, "auth", "forbidden"));
+                    return true;
+                end
             end
         end
     end