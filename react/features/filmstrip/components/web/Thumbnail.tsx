INKLUSIVACall/inklusiva-meet

commit 26943672e7ddeafb7ad1773593f6f7bdc7b00302

fix(test) Switch to using 'streamingStatus' on the track for checking… (#13055)
* fix(test) Switch to using 'streamingStatus' on the track for checking if video is received

* fix lint error

react/features/filmstrip/components/web/Thumbnail.tsx
@@ -29,10 +29,9 @@ import {
 } from '../../../base/participants/functions';
 import { IParticipant } from '../../../base/participants/types';
 import { ASPECT_RATIO_NARROW } from '../../../base/responsive-ui/constants';
-import { isTestModeEnabled } from '../../../base/testing/functions';
 // @ts-ignore
 import { Tooltip } from '../../../base/tooltip';
-import { trackStreamingStatusChanged, updateLastTrackVideoMediaEvent } from '../../../base/tracks/actions';
+import { trackStreamingStatusChanged } from '../../../base/tracks/actions';
 import {
     getLocalAudioTrack,
     getTrackByMediaTypeAndParticipant,
@@ -51,8 +50,7 @@ import {
     DISPLAY_VIDEO,
     FILMSTRIP_TYPE,
     SHOW_TOOLBAR_CONTEXT_MENU_AFTER,
-    THUMBNAIL_TYPE,
-    VIDEO_TEST_EVENTS
+    THUMBNAIL_TYPE
 } from '../../constants';
 import {
     computeDisplayModeFromInput,
@@ -168,11 +166,6 @@ export interface IProps extends WithTranslation {
      * Indicates whether the participant is screen sharing.
      */
     _isScreenSharing: boolean;
-
-    /**
-     * Indicates whether testing mode is enabled.
-     */
-    _isTestModeEnabled: boolean;
 
     /**
      * Indicates whether the video associated with the thumbnail is playable.
@@ -441,7 +434,6 @@ class Thumbnail extends Component<IProps, IState> {
         this.timeoutHandle = undefined;
         this.containerRef = createRef<HTMLSpanElement>();
         this._clearDoubleClickTimeout = this._clearDoubleClickTimeout.bind(this);
-        this._onCanPlay = this._onCanPlay.bind(this);
         this._onClick = this._onClick.bind(this);
         this._onTogglePinButtonKeyDown = this._onTogglePinButtonKeyDown.bind(this);
         this._onFocus = this._onFocus.bind(this);
@@ -452,7 +444,6 @@ class Thumbnail extends Component<IProps, IState> {
             trailing: false
         });
         this._onMouseLeave = this._onMouseLeave.bind(this);
-        this._onTestingEvent = this._onTestingEvent.bind(this);
         this._onTouchStart = this._onTouchStart.bind(this);
         this._onTouchEnd = this._onTouchEnd.bind(this);
         this._onTouchMove = this._onTouchMove.bind(this);
@@ -1007,41 +998,6 @@ class Thumbnail extends Component<IProps, IState> {
     }
 
     /**
-     * Canplay event listener.
-     *
-     * @param {SyntheticEvent} event - The event.
-     * @returns {void}
-     */
-    _onCanPlay(event: any) {
-        this.setState({ canPlayEventReceived: true });
-
-        const {
-            _isTestModeEnabled,
-            _videoTrack
-        } = this.props;
-
-        if (_videoTrack && _isTestModeEnabled) {
-            this._onTestingEvent(event);
-        }
-    }
-
-    /**
-     * Event handler for testing events.
-     *
-     * @param {SyntheticEvent} event - The event.
-     * @returns {void}
-     */
-    _onTestingEvent(event: any) {
-        const {
-            _videoTrack,
-            dispatch
-        } = this.props;
-        const jitsiVideoTrack = _videoTrack?.jitsiTrack;
-
-        dispatch(updateLastTrackVideoMediaEvent(jitsiVideoTrack, event.type));
-    }
-
-    /**
      * Renders a remote participant's 'thumbnail.
      *
      * @param {boolean} local - Whether or not it's the local participant.
@@ -1055,7 +1011,6 @@ class Thumbnail extends Component<IProps, IState> {
             _isMobile,
             _isMobilePortrait,
             _isScreenSharing,
-            _isTestModeEnabled,
             _localFlipX,
             _participant,
             _shouldDisplayTintBackground,
@@ -1083,13 +1038,6 @@ class Thumbnail extends Component<IProps, IState> {
                 styles.thumbnail.height = styles.thumbnail.width;
                 containerClassName = `${containerClassName} self-view-mobile-portrait`;
             }
-        } else {
-            if (_videoTrack && _isTestModeEnabled) {
-                VIDEO_TEST_EVENTS.forEach(attribute => {
-                    videoEventListeners[attribute] = this._onTestingEvent;
-                });
-            }
-            videoEventListeners.onCanPlay = this._onCanPlay;
         }
 
         const video = _videoTrack && <VideoTrack
@@ -1204,12 +1152,10 @@ class Thumbnail extends Component<IProps, IState> {
      */
     render() {
         const {
-            _isTestModeEnabled,
             _isVirtualScreenshareParticipant,
             _participant,
             _shouldDisplayTintBackground
         } = this.props;
-        const videoEventListeners: any = {};
 
         if (!_participant) {
             return null;
@@ -1231,13 +1177,6 @@ class Thumbnail extends Component<IProps, IState> {
         if (_isVirtualScreenshareParticipant) {
             const { isHovered } = this.state;
             const { _videoTrack, _isMobile, classes, _thumbnailType } = this.props;
-
-            if (_isTestModeEnabled) {
-                VIDEO_TEST_EVENTS.forEach(attribute => {
-                    videoEventListeners[attribute] = this._onTestingEvent;
-                });
-                videoEventListeners.onCanPlay = this._onCanPlay;
-            }
 
             return (
                 <VirtualScreenshareParticipant
@@ -1404,7 +1343,6 @@ function _mapStateToProps(state: IReduxState, ownProps: any): Object {
         _isMobile,
         _isMobilePortrait,
         _isScreenSharing: _videoTrack?.videoType === 'desktop',
-        _isTestModeEnabled: isTestModeEnabled(state),
         _isVideoPlayable: id && isVideoPlayable(state, id),
         _isVirtualScreenshareParticipant,
         _localFlipX: Boolean(localFlipX),

modules/UI/videolayout/VideoContainer.js
@@ -6,9 +6,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { browser } from '../../../react/features/base/lib-jitsi-meet';
-import { isTestModeEnabled } from '../../../react/features/base/testing';
 import { FILMSTRIP_BREAKPOINT } from '../../../react/features/filmstrip';
-import { LargeVideoBackground, ORIENTATION, updateLastLargeVideoMediaEvent } from '../../../react/features/large-video';
+import { LargeVideoBackground, ORIENTATION } from '../../../react/features/large-video';
 import { setLargeVideoDimensions } from '../../../react/features/large-video/actions.any';
 import { LAYOUTS, getCurrentLayout } from '../../../react/features/video-layout';
 /* eslint-enable no-unused-vars */
@@ -21,15 +20,6 @@ import LargeContainer from './LargeContainer';
 export const VIDEO_CONTAINER_TYPE = 'camera';
 
 const FADE_DURATION_MS = 300;
-
-/**
- * List of container events that we are going to process, will be added as listener to the
- * container for every event in the list. The latest event will be stored in redux.
- */
-const containerEvents = [
-    'abort', 'canplay', 'canplaythrough', 'emptied', 'ended', 'error', 'loadeddata', 'loadedmetadata', 'loadstart',
-    'pause', 'play', 'playing', 'ratechange', 'stalled', 'suspend', 'waiting'
-];
 
 /**
  * Returns an array of the video dimensions, so that it keeps it's aspect
@@ -266,14 +256,6 @@ export class VideoContainer extends LargeContainer {
         this._resizeListeners = new Set();
 
         this.$video[0].onresize = this._onResize.bind(this);
-
-        if (isTestModeEnabled(APP.store.getState())) {
-            const cb = name => APP.store.dispatch(updateLastLargeVideoMediaEvent(name));
-
-            containerEvents.forEach(event => {
-                this.$video[0].addEventListener(event, cb.bind(this, event));
-            });
-        }
     }
 
     /**

react/features/base/testing/functions.ts
@@ -1,4 +1,5 @@
 import { IReduxState, IStore } from '../../app/types';
+import { isTrackStreamingStatusActive } from '../../connection-indicator/functions';
 import { MEDIA_TYPE, VIDEO_TYPE } from '../media/constants';
 import { getParticipantById, isScreenShareParticipant } from '../participants/functions';
 import { getTrackByMediaTypeAndParticipant, getVideoTrackByParticipant } from '../tracks/functions';
@@ -46,10 +47,8 @@ export function isLargeVideoReceived({ getState }: IStore): boolean {
     const largeVideoParticipantId = state['features/large-video'].participantId ?? '';
     const largeVideoParticipant = getParticipantById(state, largeVideoParticipantId ?? '');
     const videoTrack = getVideoTrackByParticipant(state, largeVideoParticipant);
-    const lastMediaEvent = state['features/large-video']?.lastMediaEvent;
 
-    return Boolean(videoTrack && !videoTrack.muted
-        && (lastMediaEvent === 'playing' || lastMediaEvent === 'canplaythrough'));
+    return Boolean(videoTrack && !videoTrack.muted && isTrackStreamingStatusActive(videoTrack));
 }
 
 /**
@@ -63,8 +62,6 @@ export function isRemoteVideoReceived({ getState }: IStore, id: string): boolean
     const state = getState();
     const participant = getParticipantById(state, id);
     const videoTrack = getVideoTrackByParticipant(state, participant);
-    const lastMediaEvent = videoTrack?.lastMediaEvent;
 
-    return Boolean(videoTrack && !videoTrack.muted
-        && (lastMediaEvent === 'playing' || lastMediaEvent === 'canplaythrough'));
+    return Boolean(videoTrack && !videoTrack.muted && isTrackStreamingStatusActive(videoTrack));
 }

react/features/base/tracks/actionTypes.ts
@@ -126,14 +126,3 @@ export const TRACK_UPDATED = 'TRACK_UPDATED';
  * }
  */
 export const TRACK_WILL_CREATE = 'TRACK_WILL_CREATE';
-
-/**
- * Action to update the redux store with the current media event name of the video track.
- *
- * @returns {{
- *     type: TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT,
- *     track: Track,
- *     name: string
- * }}
- */
-export const TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT = 'TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT';

react/features/base/tracks/actions.any.ts
@@ -31,7 +31,6 @@ import {
     TRACK_REMOVED,
     TRACK_STOPPED,
     TRACK_UPDATED,
-    TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT,
     TRACK_WILL_CREATE
 } from './actionTypes';
 import {
@@ -823,29 +822,6 @@ export function setNoSrcDataNotificationUid(uid?: string) {
     return {
         type: SET_NO_SRC_DATA_NOTIFICATION_UID,
         uid
-    };
-}
-
-/**
- * Updates the last media event received for a video track.
- *
- * @param {JitsiRemoteTrack} track - JitsiTrack instance.
- * @param {string} name - The current media event name for the video.
- * @returns {{
- *     type: TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT,
- *     track: Track,
- *     name: string
- * }}
- */
-export function updateLastTrackVideoMediaEvent(track: any, name: string): {
-    name: string;
-    track: any;
-    type: 'TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT';
-} {
-    return {
-        type: TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT,
-        track,
-        name
     };
 }
 

react/features/base/tracks/reducer.ts
@@ -11,7 +11,6 @@ import {
     TRACK_OWNER_CHANGED,
     TRACK_REMOVED,
     TRACK_UPDATED,
-    TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT,
     TRACK_WILL_CREATE
 } from './actionTypes';
 import { ITrack } from './types';
@@ -73,20 +72,7 @@ function track(state: ITrack, action: any) {
         }
         break;
     }
-    case TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT: {
-        const t = action.track;
 
-        if (state.jitsiTrack === t) {
-            if (state.lastMediaEvent !== action.name) {
-
-                return {
-                    ...state,
-                    lastMediaEvent: action.name
-                };
-            }
-        }
-        break;
-    }
     case TRACK_NO_DATA_FROM_SOURCE: {
         const t = action.track;
 
@@ -117,7 +103,6 @@ ReducerRegistry.register<ITracksState>('features/base/tracks', (state = [], acti
     case PARTICIPANT_ID_CHANGED:
     case TRACK_NO_DATA_FROM_SOURCE:
     case TRACK_OWNER_CHANGED:
-    case TRACK_UPDATE_LAST_VIDEO_MEDIA_EVENT:
     case TRACK_UPDATED:
         return state.map((t: ITrack) => track(t, action));
     case TRACK_ADDED: {

react/features/base/tracks/types.ts
@@ -46,7 +46,6 @@ export interface ITrackOptions {
 export interface ITrack {
     isReceivingData: boolean;
     jitsiTrack: any;
-    lastMediaEvent?: string;
     local: boolean;
     mediaType: MediaType;
     mirror: boolean;

react/features/filmstrip/constants.ts
@@ -64,30 +64,6 @@ export const DEFAULT_MAX_COLUMNS = 5;
 export const ABSOLUTE_MAX_COLUMNS = 7;
 
 /**
- * An array of attributes of the video element that will be used for adding a listener for every event in the list.
- * The latest event will be stored in redux. This is currently used by torture only.
- */
-export const VIDEO_TEST_EVENTS = [
-    'onAbort',
-    'onCanPlay',
-    'onCanPlayThrough',
-    'onEmptied',
-    'onEnded',
-    'onError',
-    'onLoadedData',
-    'onLoadedMetadata',
-    'onLoadStart',
-    'onPause',
-    'onPlay',
-    'onPlaying',
-    'onRateChange',
-    'onStalled',
-    'onSuspend',
-    'onWaiting'
-];
-
-
-/**
  * Display mode constant used when video is being displayed on the small video.
  *
  * @type {number}

commit d75b9ecaec1758d8a50abe0b734cae6810b19fb8

fix(thumbnail) Fix broken video for thumbnail. Fixes a regression introduced by https://github.com/jitsi/jitsi-meet/commit/26943672e7ddeafb7ad1773593f6f7bdc7b00302

react/features/filmstrip/components/web/Thumbnail.tsx
@@ -434,6 +434,7 @@ class Thumbnail extends Component<IProps, IState> {
         this.timeoutHandle = undefined;
         this.containerRef = createRef<HTMLSpanElement>();
         this._clearDoubleClickTimeout = this._clearDoubleClickTimeout.bind(this);
+        this._onCanPlay = this._onCanPlay.bind(this);
         this._onClick = this._onClick.bind(this);
         this._onTogglePinButtonKeyDown = this._onTogglePinButtonKeyDown.bind(this);
         this._onFocus = this._onFocus.bind(this);
@@ -998,6 +999,15 @@ class Thumbnail extends Component<IProps, IState> {
     }
 
     /**
+     * Canplay event listener.
+     *
+     * @returns {void}
+     */
+    _onCanPlay() {
+        this.setState({ canPlayEventReceived: true });
+    }
+
+    /**
      * Renders a remote participant's 'thumbnail.
      *
      * @param {boolean} local - Whether or not it's the local participant.
@@ -1038,6 +1048,8 @@ class Thumbnail extends Component<IProps, IState> {
                 styles.thumbnail.height = styles.thumbnail.width;
                 containerClassName = `${containerClassName} self-view-mobile-portrait`;
             }
+        } else {
+            videoEventListeners.onCanPlay = this._onCanPlay;
         }
 
         const video = _videoTrack && <VideoTrack