6c087dff9e9c7c32ded60cc5c50ade5a998bd495
Oliver <or@4morgen.de>
2023-09-19 16:57:06 +0200

    distressbutton dimming

---
+++ react/features/inklusiva/sessiondata/middleware.ts
@@ -0,0 +1,39 @@
+import { SET_INDISTRESS_DISABLED, SET_INDISTRESS_ENABLED } from "./actionTypes";
+import MiddlewareRegistry from "../../base/redux/MiddlewareRegistry";
+import { setParticipantOpacitySetting } from "../../filmstrip/actions.web";
+
+/**
+ * The middleware of the feature Filmstrip.
+ */
+MiddlewareRegistry.register((store) => (next) => (action) => {
+    const state = store.getState();
+    const userData = state["features/lag/userdata"];
+    const dimmingValue = userData?.distressbutton?.dimming ?? 50;
+
+    if (action.type === SET_INDISTRESS_ENABLED) {
+        state["features/filmstrip"].remoteParticipants.forEach(
+            (participantId: string) => {
+                store.dispatch(
+                    setParticipantOpacitySetting(
+                        false,
+                        participantId,
+                        dimmingValue / 100.0,
+                    ),
+                );
+            },
+        );
+        // setParticipantOpacitySetting(true, null, dimmingValue / 100.0);
+    }
+
+    if (action.type === SET_INDISTRESS_DISABLED) {
+        state["features/filmstrip"].remoteParticipants.forEach(
+            (participantId: string) => {
+                store.dispatch(
+                    setParticipantOpacitySetting(false, participantId, 0),
+                );
+            },
+        );
+    }
+
+    return next(action);
+});
