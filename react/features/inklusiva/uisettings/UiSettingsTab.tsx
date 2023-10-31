7676fbe7ac0ff1f70eb4ed4c76f12fb07a7ae610
Marvin Kretzschmar <marvin@MacMini2023.local>
2023-10-31 15:44:33 +0100

    Einstellungen Benutzeroberfläche fertig gestellt

--- react/features/inklusiva/uisettings/UiSettingsTab.tsx
+++ react/features/inklusiva/uisettings/UiSettingsTab.tsx
@@ -7,6 +7,7 @@ import AbstractDialogTab, {
     IProps as AbstractDialogTabProps
 } from '../../base/dialog/components/web/AbstractDialogTab';
 import { translate } from '../../base/i18n/functions';
+import Checkbox from '../../base/ui/components/web/Checkbox';
 import Slider from '../../base/ui/components/web/Slider';
 
 
@@ -98,13 +99,10 @@ class UiSettingsTab extends AbstractDialogTab<IProps, any> {
      * @returns {ReactElement}
      */
     render() {
-        const { classes, fontSize, t } = this.props;
+        const { classes, fontSize, iconSize, visualCues, acousticCues, t } = this.props;
 
         return (
             <div className = { classes.container }>
-                <div className = { classes.inputElement }>
-                    {t('settings.uiSettings.Sizes')}
-                </div>
                 <div className = { classes.inputblockContainer }>
                     <Slider
                         className = { classes.inputElement }
@@ -119,11 +117,67 @@ class UiSettingsTab extends AbstractDialogTab<IProps, any> {
                         }
                         step = { 1 } />
                 </div>
+                <div className = { classes.inputblockContainer }>
+                    <Slider
+                        className = { classes.inputElement }
+                        defaultValue = { iconSize }
+                        label = { t('settings.uiSettings.iconSize') }
+                        max = { 2 }
+                        min = { 0 }
+                        onChange = { event =>
+                            super._onChange({
+                                iconSize: parseInt(event.target.value, 10)
+                            })
+                        }
+                        step = { 1 } />
+                </div>
+                <div className = { classes.inputblockContainer }>
+                    <div>
+                        <b className = { classes.headline }>
+                            {t('settings.uiSettings.acousticcues.headline')}
+                        </b>
+                    </div>
+                    <Checkbox
+                        checked = { acousticCues }
+                        className = { classes.inputElement }
+                        label = { t('settings.uiSettings.acousticcues.label') }
+                        // eslint-disable-next-line react/jsx-no-bind
+                        name = 'acousticcues_enable'
+                        onChange = { () =>
+                            super._onChange({
+                                acousticCues: !acousticCues
+                            })
+                        } />
+
+                    <div className = { classes.description }>
+                        {t('settings.uiSettings.acousticcues.description')}
+                    </div>
+                </div>
+                <div className = { classes.inputblockContainer }>
+                    <div>
+                        <b className = { classes.headline }>
+                            {t('settings.uiSettings.visualcues.headline')}
+                        </b>
+                    </div>
+                    <Checkbox
+                        checked = { visualCues }
+                        className = { classes.inputElement }
+                        label = { t('settings.uiSettings.visualcues.label') }
+                        // eslint-disable-next-line react/jsx-no-bind
+                        name = 'visualcues_enable'
+                        onChange = { () =>
+                            super._onChange({
+                                visualCues: !visualCues
+                            })
+                        } />
+
+                    <div className = { classes.description }>
+                        {t('settings.uiSettings.visualcues.description')}
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
 export default withStyles(styles)(translate(UiSettingsTab));
-
-// export default translate(UiSettingsTab);
