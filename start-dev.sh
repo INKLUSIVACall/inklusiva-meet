598dab5589ec03ac46e9d85f03f585ecbcdc41b7
Julian Becker <jb@4morgen.de>
2023-11-20 13:29:57 +0100

    Inklusiva-Meet-Rollen-Management.

--- start-dev.sh
+++ start-dev.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Rebuild lib-jitsi-meet
-cd node_modules/lib-jitsi-meet
-npm run build
-cd ../..
+#cd node_modules/lib-jitsi-meet
+#npm run watch &
+#cd ../..
 
 # Start the development server
 export WEBPACK_DEV_SERVER_PROXY_TARGET=https://jitsi.intern.4morgen.de
 make dev
+#npm run start
