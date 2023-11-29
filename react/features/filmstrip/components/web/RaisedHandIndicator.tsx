9795501b943e226a293f0cbcd53c7edd408b17b4
Marvin Kretzschmar <marvin@MacMini2023.local>
2023-11-29 11:41:42 +0100

    Handheben zaehlen auch auf Filmstrip implementiert

--- react/features/filmstrip/components/web/RaisedHandIndicator.tsx
+++ react/features/filmstrip/components/web/RaisedHandIndicator.tsx
@@ -8,6 +8,7 @@ import { getParticipantById, hasRaisedHand } from '../../../base/participants/fu
 import { IParticipant } from '../../../base/participants/types';
 import BaseIndicator from '../../../base/react/components/web/BaseIndicator';
 import { TOOLTIP_POSITION } from '../../../base/ui/constants.any';
+import { getRaisedHandPosition } from '../../../participants-pane/functions';
 
 /**
  * The type of the React {@code Component} props of {@link RaisedHandIndicator}.
@@ -37,9 +38,15 @@ const useStyles = makeStyles()(theme => {
             backgroundColor: theme.palette.warning02,
             padding: '4px',
             zIndex: 3,
-            display: 'inline-block',
+            display: 'flex',
             borderRadius: '4px',
             boxSizing: 'border-box'
+        },
+        indicatorText: {
+            color: theme.palette.icon04,
+            flexGrow: 1,
+            fontSize: '0.75rem',
+            marginLeft: '8px'
         }
     };
 });
@@ -58,6 +65,8 @@ const RaisedHandIndicator = ({
         getParticipantById(state, participantId));
     const _raisedHand = hasRaisedHand(participant);
     const { classes: styles, theme } = useStyles();
+    const raisedHandPosition: number | undefined = useSelector((state: IReduxState) =>
+        getRaisedHandPosition(state, participantId));
 
     if (!_raisedHand) {
         return null;
@@ -71,6 +80,7 @@ const RaisedHandIndicator = ({
                 iconSize = { `${iconSize}px` }
                 tooltipKey = 'raisedHand'
                 tooltipPosition = { tooltipPosition } />
+            <span className = { styles.indicatorText }>{ raisedHandPosition }</span>
         </div>
     );
 };
