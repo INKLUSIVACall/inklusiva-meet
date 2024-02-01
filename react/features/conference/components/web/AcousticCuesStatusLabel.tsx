e818e44e1919bc72b2c75218b26b13c60dfe169e
Oliver <or@4morgen.de>
2024-02-01 10:32:20 +0100

    #78 #79

--- react/features/conference/components/web/AcousticCuesStatusLabel.tsx
+++ react/features/conference/components/web/AcousticCuesStatusLabel.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from 'tss-react/mui';
 
+import { openDialog } from '../../../base/dialog/actions';
 import { IconVolumeOff } from '../../../base/icons/svg';
 import Label from '../../../base/label/components/web/Label';
 import Tooltip from '../../../base/tooltip/components/Tooltip';
+import ReenableAudioCuesDialog from '../../../inklusiva/audiosettings/components/ReenableAudioCuesDialog';
 import { isAcousticCuesEnabled } from '../../../inklusiva/uisettings/functions';
 
 
-const useStyles = makeStyles()(theme => {
+const useStyles = makeStyles()(() => {
     return {
         acousticCuesStatusLabel: {
             backgroundColor: 'transparent',
@@ -26,6 +28,12 @@ const useStyles = makeStyles()(theme => {
 const AcousticCuesStatusLabel = () => {
     const acousticCuesStatus = useSelector(isAcousticCuesEnabled);
     const { classes } = useStyles();
+    const dispatch = useDispatch();
+
+    const onClick = () => {
+        dispatch(openDialog(ReenableAudioCuesDialog, { }));
+    };
+
 
     if (!acousticCuesStatus) {
         return (
@@ -38,7 +46,8 @@ const AcousticCuesStatusLabel = () => {
                     icon = { IconVolumeOff }
                     iconColor = '#fff'
                     iconSize = { '24' }
-                    id = 'acousticCuesStatusLabel' />
+                    id = 'acousticCuesStatusLabel'
+                    onClick = { onClick } />
             </Tooltip>
         );
     }
